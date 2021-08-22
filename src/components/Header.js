832569be9c4cc2f94df681a428084ce2abb00555
[2기-1반]김희재 <gmlwo0109@gmail.com>
2021-08-23 00:16:05 +0900

    current covid-19 with React

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>COVID-19</h1>
+                <select>
+                    <option>국내</option>
+                    <option>해외</option>
+                </select>
+        </div>
+    )
+}
+
+export default Header
